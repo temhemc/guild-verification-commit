8d685e609ba8c6130fff78793917fb3e873b0874
Mehmet Çelik <funstarpark@gmail.com>
2025-10-31 00:08:25 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-10-30T21:08:23.677Z
-Commit: Fix race condition in async operations
+Timestamp: 2025-10-30T21:08:28.837Z
+Commit: Fix CSS styling bug on mobile devices
