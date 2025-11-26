ab9d72de59d1efdca74c4ddbbbe29498f7070874
Mehmet Çelik <funstarpark@gmail.com>
2025-11-27 01:27:41 +0300

    Add export functionality for reports

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-26T22:27:28.758Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2025-11-26T22:27:40.839Z
+Commit: Add export functionality for reports
