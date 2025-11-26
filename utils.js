c67df120dfa00d6b7e2308ce47ef7d609f73c972
Mehmet Çelik <funstarpark@gmail.com>
2025-11-27 01:25:44 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-26T22:25:39.423Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2025-11-26T22:25:44.047Z
+Commit: Resolve null pointer exception in user service
