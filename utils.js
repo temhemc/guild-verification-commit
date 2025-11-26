a0e37c293768c452d1a85816e9ce88132bbfb41c
Mehmet Çelik <funstarpark@gmail.com>
2025-11-27 01:27:15 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-26T22:26:14.088Z
-Commit: Patch API endpoint error handling
+Timestamp: 2025-11-26T22:27:14.895Z
+Commit: Fix memory leak in event handlers
